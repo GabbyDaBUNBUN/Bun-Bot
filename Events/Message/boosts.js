6fc1e0da81111cce3d86dd535241475cc56fa105
GabbyDaBUNBUN <bunbun@expressionbot.xyz>
2023-06-18 16:40:12 -0500

    u

--- Events/Message/boosts.js
+++ Events/Message/boosts.js
@@ -27,7 +27,7 @@ module.exports = {
                     new EmbedBuilder()
                         .setColor(color)
                         .setTitle("New Booster")
-                        .setDescription(`Thank you ${author} for boosting!!! We are up to ${guild.premiumSubscriptionCount} boosts!!! You get some awesome rewards for doing so!!! You will receive a custom role, as well as a custom chat room!!! Please <#1037971276272242688> and let us know what you would like for your role name, color (in hex), and emoji (Must be from this server). We will also need to know what you want the name of your Custom Chat Room to be!!`)
+                        .setDescription(`Thank you ${newMember.id} for boosting!!! We are up to ${guild.premiumSubscriptionCount} boosts!!! You get some awesome rewards for doing so!!! You will receive a custom role, as well as a custom chat room!!! Please <#1037971276272242688> and let us know what you would like for your role name, color (in hex), and emoji (Must be from this server). We will also need to know what you want the name of your Custom Chat Room to be!!`)
                         .setImage(`https://ucarecdn.com/b8f01ed8-11c0-48b3-83c5-3dec4734adc4/b1e248b66c17a7da87cdf29e24b38e34.jpg`)
                         .setFooter({ text: "Boosts by Bun Bot" })
                         .setTimestamp()
